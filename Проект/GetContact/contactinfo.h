6a04095ea754c528bbe0a504434785315a9c5c19
leukhin85 <86623915+leukhin85@users.noreply.github.com>
2021-07-09 10:50:45 +0300

    Add files via upload

---
+++ Проект/GetContact/contactinfo.h
@@ -0,0 +1,43 @@
+#ifndef CONTACTINFO_H
+#define CONTACTINFO_H
+
+#include <QWidget>
+#include "mainwindow.h"
+
+namespace Ui {
+class ContactInfo;
+}
+
+class ContactInfo : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ContactInfo(QWidget *parent = nullptr);  //Конструкторы класса
+             ContactInfo(Contact l_contact, QWidget *parent = nullptr);
+             ~ContactInfo();
+
+    inline Contact getContact() {return contact;}
+
+    void setContactInfo();                   //Обновление информации о контакте на виджете
+    void setContactInfo(Contact l_contact);  //Перегрузка, делает то же самое, но также переопределяет contact
+
+    QString getName() {return contact.getName(); } //геттеры
+    QString getNum() {return contact.getNum(); }
+
+private slots:
+    void on_pushButton_clicked(); //Бинд кнопки редактирования
+
+    void on_pushButton_2_clicked();
+
+    void on_btn_red_clicked();
+
+    void on_btn_delete_clicked();
+
+private:
+    Ui::ContactInfo *ui;
+
+    Contact contact; //Контакт, который отображается в форме
+};
+
+#endif // CONTACTINFO_H
